f5a757f451c68d9cff22a7fd9673f371ab2d1f2a
20044002 <20044002@LAPTOP-LBFB95U0>
2021-08-11 16:18:07 +0800

    update viewMenu() in C206_CaseStudy

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,96 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
 
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ArrayList<Menu> MenuList = new ArrayList<Menu>();
+
+		MenuList.add(new Menu("Jan","Chicken Rice", "$5", "Coke"));
+		MenuList.add(new Menu("Feb","Fish and Chips", "$6", "Sprite"));
+		MenuList.add(new Menu("Mar","Fish Soup", "$4.50", "Milo"));
+		
+		int option = 0;
+
+		while (option != 4) {
+
+			C206_CaseStudy.menu();
+			option = Helper.readInt("Enter an option > ");
+
+			if (option == 1) {
+				// Add menu
+				Menu m = inputMenu();
+				C206_CaseStudy.addMenu(MenuList, m);
+
+			} else if (option == 2) {
+				//view menu
+				C206_CaseStudy.viewMenu(MenuList);
+				
+			} else if (option == 3) {
+				//Delete menu
+				//C206_CaseStudy.deleteMenu();
+				
+			} else if (option == 4) {
+				System.out.println("Bye!");
+			} else {
+				System.out.println("Invalid option");
+			}
+
+		}
 
 	}
 
+
+	public static void menu() {
+		C206_CaseStudy.setHeader("Monthly Menu");
+		System.out.println("1. Add Monthly menu");
+		System.out.println("2. View Monthly menu");
+		System.out.println("3. Delete Menu");
+		System.out.println("4. Quit");
+		Helper.line(40, "-");
+
+	}
+	
+	public static void setHeader(String header) {
+		Helper.line(40, "-");
+		System.out.println(header);
+		Helper.line(40, "-");
+	}
+	
+	public static String retrieveMenu(ArrayList<Menu> MenuList) {
+		String output = "";
+
+		for (int i = 0; i < MenuList.size(); i++) {
+
+			output += String.format("%-20s %-20s %-20s %-20s\n", MenuList.get(i).getMonth(), MenuList.get(i).getFood(),
+					MenuList.get(i).getPrice(), MenuList.get(i).getDrinks());
+		}
+		return output;
+	}
+	
+	public static void viewMenu(ArrayList<Menu> MenuList) {
+		C206_CaseStudy.setHeader("Menu");
+		String output = String.format("%-20s %-20s %-20s %-20s\n", "Month", "FOOD", "PRICE", "DRINKS");
+		 output += retrieveMenu(MenuList);	
+		System.out.println(output);
+	}
+		
+		public static Menu inputMenu() {
+			String month = Helper.readString("Enter Month > ");
+			String food = Helper.readString("Enter Food > ");
+			String price = Helper.readString("Enter Price > ");
+			String drinks = Helper.readString("Enter Drinks > ");
+
+			Menu m= new Menu(month, food, price, drinks);
+			return m;
+			
+		}
+		public static void addMenu(ArrayList<Menu> MenuList, Menu m) {
+			
+			MenuList.add(m);
+			System.out.println("Menu added!");
+		}
+		
 }
--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,17 +1,84 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
-
+	private Menu JanMenu;
+	private Menu FebMenu;
+	private Menu MarMenu;
+	
+	private ArrayList<Menu> MenuList;
+	
+	public C206_CaseStudyTest() {
+		super();
+	}
+	
 	@Before
 	public void setUp() throws Exception {
+		//test data for viewMenu
+		JanMenu = new Menu("Jan", "Chicken Rice", "$5", "Coke");
+		FebMenu = new Menu("Feb", "Fish and Chips", "$6", "Sprite");
+		MarMenu = new Menu("Mar", "Fish Soup", "$4.50", "Milo");
+		
+		MenuList = new ArrayList<Menu>();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testAddMenu() {
+		// Item list is not null, so that can add a new item
+		assertNotNull("Test if there is valid Camcorder arraylist to add to", MenuList);
+
+		// Given an empty list, after adding 1 item, the size of the list is 1
+		C206_CaseStudy.addMenu(MenuList, JanMenu);
+		assertEquals("Test if that Menu arraylist size is 1?", 1, MenuList.size());
+
+		// The item just added is as same as the first item of the list
+		assertSame("Test that Menu is added same as 1st item of the list?", JanMenu, MenuList.get(0));
+
+		// Add another item. test The size of the list is 2?
+		C206_CaseStudy.addMenu(MenuList, FebMenu);
+		assertEquals("Test that Menu arraylist size is 2?", 2, MenuList.size());
+		assertSame("Test that Menu is added same as 1st item of the list?", FebMenu, MenuList.get(0));
+		
+		// Add another item. test The size of the list is 3?
+		C206_CaseStudy.addMenu(MenuList, MarMenu);
+		assertEquals("Test that Menu arraylist size is 3?", 3, MenuList.size());
+		assertSame("Test that Menu is added same as 1st item of the list?", MarMenu, MenuList.get(0));
+	}
+	
+	
+	@Test
+	public void testRetrieveMenu() {
+		// Test if Item list is not null but empty, so that can add a new item
+		assertNotNull("Test if there is valid Menu arraylist to add to", MenuList);
+
+		// test if the list of Menu retrieved from C206_CaseStudy is empty
+		String retrieveMenu = C206_CaseStudy.retrieveMenu(MenuList);
+		String testOutput = "";
+		assertEquals("Check that Menulist", testOutput, retrieveMenu);
+
+		// Given an empty list, after adding 2 items, test if the size of the list is 2
+		C206_CaseStudy.addMenu(MenuList, JanMenu);
+		C206_CaseStudy.addMenu(MenuList, FebMenu);
+		assertEquals("Test if that Menu arraylist size is 2?", 2, MenuList.size());
+
+		// test if the expected output string same as the list of menu retrieved
+		// from C206_CaseStudy
+		retrieveMenu = C206_CaseStudy.retrieveMenu(MenuList);
+
+		testOutput = String.format("%-20s %-20s %-20s %-20s\\n", "Jan", "Chicken Rice", "$5", "Coke");
+		testOutput += String.format("%-20s %-20s %-20s %-20s\\n", "Feb", "Fish and Chips", "$6", "Sprite");
+
+		assertEquals("Check that ViewAllCamcorderlist", testOutput, retrieveMenu);
+
 	}
 
 	@Test
