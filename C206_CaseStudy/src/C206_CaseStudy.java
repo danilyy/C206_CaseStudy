19252bfd6aad66f70eed8df1dd09f2b8746e7cad
Ansonwoo24 <87629604+Ansonwoo24@users.noreply.github.com>
2021-08-14 19:15:02 +0800

    Add files via upload
    Updated with delete function

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,133 +1,111 @@
 import java.util.ArrayList;
 
-/**
- 
-* I declare that this code was written by me.
- * I will not copy or allow others to copy my code.
- * I understand that copying code is considered as plagiarism.
- *
- * 20038360, 11 Aug 2021 11:40:03 pm
- */
-
-/**
- * I declare that this code was written by me. I will not copy or allow others
- * to copy my code. I understand that copying code is considered as plagiarism.
- *
- * 20038360, 11 Aug 2021 11:40:03 pm
- */
 public class C206_CaseStudy {
 
-	/**
-	 * @param args
-	 */
+	// 20044002, Anson.
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ArrayList<Menu> MenuList = new ArrayList<Menu>();
 
-		//Arraylist for menu items
-		
-		ArrayList<Items> itemList = new ArrayList<Items>();
-		
-		itemList.add(new Items(1, "WesternFood", "Spaghetti", 3.50));
-		itemList.add(new Items(2, "WesternFood", "Baked Rice", 4.50));
+		MenuList.add(new Menu("Jan","Chicken Rice", "$5", "Coke"));
+		MenuList.add(new Menu("Feb","Fish and Chips", "$6", "Sprite"));
+		MenuList.add(new Menu("Mar","Fish Soup", "$4.50", "Milo"));
 		
 		int option = 0;
-		
+
 		while (option != 4) {
-			menu();
+//test commit
+			C206_CaseStudy.menu();
 			option = Helper.readInt("Enter an option > ");
 
 			if (option == 1) {
-				//view menu item
-				C206_CaseStudy.viewItems(itemList);
-			}
-			
-			else if (option == 2) {
-				Items item = inputItem();
-				C206_CaseStudy.addItem(itemList, item);
-			}
-			else if (option == 3) {
-				C206_CaseStudy.deleteItem(itemList);
-			}
-			else if (option == 4) {
+				// Add menu
+				Menu m = inputMenu();
+				C206_CaseStudy.addMenu(MenuList, m);
+
+			} else if (option == 2) {
+				//view menu
+				C206_CaseStudy.viewMenu(MenuList);
+				
+			} else if (option == 3) {
+				//Delete menu
+				C206_CaseStudy.deleteMenu(MenuList);
+				
+			} else if (option == 4) {
 				System.out.println("Bye!");
 			} else {
 				System.out.println("Invalid option");
 			}
-		
+
 		}
+
 	}
 
+
 	public static void menu() {
-		C206_CaseStudy.setHeader("Menu Items");
-		System.out.println("1. View Menu Items");
-		System.out.println("2. Add Menu Items");
-		System.out.println("3. Delete Menu Items");
+		C206_CaseStudy.setHeader("Monthly Menu");
+		System.out.println("1. Add Monthly menu");
+		System.out.println("2. View Monthly menu");
+		System.out.println("3. Delete Menu");
 		System.out.println("4. Quit");
-		Helper.line(50, "-");
+		Helper.line(40, "-");
 
 	}
 	
 	public static void setHeader(String header) {
-		Helper.line(80, "-");
+		Helper.line(40, "-");
 		System.out.println(header);
-		Helper.line(80, "-");
+		Helper.line(40, "-");
 	}
-	//create view items
-	public static String retrieveItems(ArrayList<Items> itemList) {
+	
+	public static String retrieveMenu(ArrayList<Menu> MenuList) {
 		String output = "";
-		
-		for (int i = 0; i < itemList.size(); i++) {
 
-			output += String.format("%-10s %-30s %-10s %-10.2fs\n", itemList.get(i).getItemID(),
-					itemList.get(i).getCategory(), itemList.get(i).getName(), itemList.get(i).getPrice());
+		for (int i = 0; i < MenuList.size(); i++) {
+
+			output += String.format("%-20s %-20s %-20s %-20s\n", MenuList.get(i).getMonth(), MenuList.get(i).getFood(),
+					MenuList.get(i).getPrice(), MenuList.get(i).getDrinks());
 		}
 		return output;
 	}
 	
-	public static void viewItems(ArrayList<Items> itemList) {
-		C206_CaseStudy.setHeader("View Items");
-		String output = String.format("%-10s %-30s %-10s  %-10s\n", "ITEM ID", "CATEGORY",
-				"NAME", "PRICE");
-		 output += retrieveItems(itemList);	
+	public static void viewMenu(ArrayList<Menu> MenuList) {
+		C206_CaseStudy.setHeader("Menu");
+		String output = String.format("%-20s %-20s %-20s %-20s\n", "Month", "FOOD", "PRICE", "DRINKS");
+		 output += retrieveMenu(MenuList);	
 		System.out.println(output);
 	}
-	// create add items
-	public static Items inputItem() {
-		int itemID = Helper.readInt("Enter item ID > ");
-		String category = Helper.readString("Enter Item Category > ");
-		String name = Helper.readString("Enter Item Name > ");
-		double price = Helper.readDouble("Enter Item Price > ");
-
-		Items item = new Items(itemID, category, name, price);
-		return item;
 		
-	}
-	public static void addItem(ArrayList<Items> itemList, Items item){
-		itemList.add(item);
-		System.out.println("Item has been successfully added!");
-	}
-	
-	// create delete items
-	public static boolean searchID(ArrayList<Items> itemList, int itemID) {
-		for (int i = 0; i < itemList.size(); i++) {
-			if(itemID == itemList.get(i).getItemID()) {
-				return true;
-			}
+		public static Menu inputMenu() {
+			String month = Helper.readString("Enter Month > ");
+			String food = Helper.readString("Enter Food > ");
+			String price = Helper.readString("Enter Price > ");
+			String drinks = Helper.readString("Enter Drinks > ");
+
+			Menu m= new Menu(month, food, price, drinks);
+			return m;
+			
 		}
-		return false;
-	}
-	
-	public static void deleteItem(ArrayList<Items> itemList){
-		// 
-		int itemID = Helper.readInt("Enter Menu ID to delete > ");
-		if(searchID(itemList, itemID)) {
-			for (int i = 0; i < itemList.size(); i++) {
-				if(itemID == itemList.get(i).getItemID()) {
-					itemList.remove(i);
-				}
-			}System.out.println("Menu Item ID"+ itemID + "has been successfully deleted!");
+		public static void addMenu(ArrayList<Menu> MenuList, Menu m) {
+			
+			MenuList.add(m);
+			System.out.println("Menu added!");
+		}
+		
+	public static void deleteMenu(ArrayList<Menu> MenuList) {
+		
+		String userInput = Helper.readString("Enter month to delete > ");
+		
+		for (int i=0; i<MenuList.size(); i++) {
+		    if (MenuList.get(i).getMonth().equals(userInput)){
+		    	MenuList.remove(i);
+			    break;
+		    }
+		   
 		}
 		
 	}
-
+		
 }
